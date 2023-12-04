9a87778ed05b0352ec6694912b84d9682d4854b0
samins <sarwar347422@gmail.com>
2023-12-03 21:41:54 -0500

    added bootstrap navbar and created dark background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,15 +13,35 @@ function App() {
   };
   return (
     <>
-      <div class='main'>C++ Learning Website</div>
+    <body class="main">
+      <Bar />
       <p > Welcome to our website! You can learn C++ by creating flashcards and solving practice problems! </p>
       <LoginForm />
       <img src="/src/assets/boxingcat.gif"></img>
       <p>catch these hands</p>
-      <a href="flashcards.html">Flashcards</a>
-      
+      </body>
     </>
   )
+}
+
+
+function Bar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>Learn C++!</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#flash">Flashcards</Nav.Link>
+            <Nav.Link href="#quiz">Quiz</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br />
+    </>
+  );
 }
 
 
