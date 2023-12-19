80ce3b1f395b624f68d30ff3fc35e4653ad5be66
samins05 <99273488+samins05@users.noreply.github.com>
2023-12-18 22:24:54 -0500

    Add files via upload

---
+++ client/src/learning.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Learn from './Learn.jsx'
+import './Learn.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Learn />
+  </React.StrictMode>,
+)
